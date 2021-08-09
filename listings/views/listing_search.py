08d63cbf193ba6352572d8933a45b0cd141b4e10
rostockerdev <rostockerdev@gmail.com>
2021-08-09 21:19:49 +0600

    :bug: Update: Bug fixes

--- listings/views/listing_search.py
+++ listings/views/listing_search.py
@@ -6,11 +6,10 @@ from listings.models import Listing
 
 def listing_search_view(request):
     qs = Listing.objects.order_by('-list_date')
-
     if 'keywords' in request.GET:
         keywords = request.GET['keywords']
         if keywords:
-            qs = Listing.objects.filter(description_icontains=keywords)
+            qs = Listing.objects.filter(description__icontains=keywords)
 
     if 'city' in request.GET:
         city = request.GET['city']
@@ -25,21 +24,18 @@ def listing_search_view(request):
     if 'bedrooms' in request.GET:
         bedrooms = request.GET['bedrooms']
         if bedrooms:
-            qs = Listing.objects.filter(bedrooms__iexact=bedrooms) 
+            qs = Listing.objects.filter(bedrooms__lte=bedrooms) 
 
     if 'price' in request.GET:
         price = request.GET['price']
         if price:
-            qs = Listing.objects.filter(price__iexact=price)
-
+            qs = Listing.objects.filter(price__lte=price)
 
     context = {
         'bedroom_choices': bedroom_choices,
         'price_choices': price_choices,
         'state_choices': state_choices,
-        'listings': qs,
-        'search_value': request.GET,
-        'listings': qs,
-        'title': 'Listings'
+        'listings': qs, 
+        'search_value': request.GET
     }
-    return render(request, 'listings/listing_search.html', context)
+    return render(request, 'listings/listing_search.html', context) 
